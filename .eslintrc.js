3fe91a13ec73c37d38965a3bb6903eee16f318a8
Dandomanos <dandomanos@gmail.com>
2018-04-05 13:11:44 +0200

    New: Add dynamic content and style from object

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
   root: true,
-  extends: ["eslint:recommended", "standard", "prettier", "prettier/standard"],
+  extends: ['eslint:recommended', 'standard', 'prettier', 'prettier/standard'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-var": 1,
-    "prefer-const": 1,
-    "no-console": 1,
+    'no-var': 1,
+    'prefer-const': 1,
+    'no-console': 1,
 
     // TODO: Remove rules above this
-    eqeqeq: 0
-  }
+    eqeqeq: 0,
+  },
 }
--- index.js
+++ index.js
@@ -1,52 +1,51 @@
-import heml from "heml";
-import fs from "fs";
-import writeFile from "./src/writeFile";
-import _ from "lodash";
+import heml from 'heml'
+import fs from 'fs'
+import writeFile from './src/writeFile'
+import _ from 'lodash'
 const options = {
-  validate: "soft", // validation levels - 'strict'|'soft'|'none'
+  validate: 'soft', // validation levels - 'strict'|'soft'|'none'
   cheerio: {}, // config passed to cheerio parser
   juice: {},
   beautify: {}, // config passed to js-beautify html method
   elements: [
     // any custom elements you want to use
-  ]
-};
+  ],
+}
 
 const style = {
-  brandingColor: "#8BD2E0",
-  backgroundsColor: "#3976BD",
-  secondaryBackgroundColor: "#BABABA",
-  fontsColor: "#FDFDFD",
-  fontSize: "16px",
-  h1FontSize: "30px"
-};
+  brandingColor: '#8BD2E0',
+  backgroundsColor: '#3976BD',
+  secondaryBackgroundColor: '#BABABA',
+  fontsColor: '#FDFDFD',
+  fontSize: '16px',
+  h1FontSize: '30px',
+}
 
 const templateContent = {
-  imageHeader:
-    "https://s3-eu-west-1.amazonaws.com/orchextra-images-pt-dev/5a1fcc5cda4a93183c915032.jpeg",
+  imageHeader: 'https://s3-eu-west-1.amazonaws.com/orchextra-images-pt-dev/5a1fcc5cda4a93183c915032.jpeg',
   title: "CONGRATULATIONS, YOU'RE A WINNER!",
-  description:
-    "You won an amazing prize. Click on the button below and claim your prize.",
-  buttonText: "Claim Prize",
-  buttonUrl: "https://heml.io"
-};
+  description: 'You won an amazing prize. Click on the button below and claim your prize.',
+  buttonText: 'Claim Prize',
+  buttonUrl: 'https://heml.io',
+}
 
-//Html template file name
-const name = process.argv[2] || "email";
+// Html template file name
+const name = process.argv[2] || 'email'
 
-if (!name) throw new Error("No template name defined");
+if (!name) throw new Error('No template name defined')
 
-//Get template content
-const template = fs
-  .readFileSync(__dirname + "/templates/" + name + ".heml")
-  .toString();
+// Get template content
+const template = fs.readFileSync(`${__dirname}/templates/${name}.heml`).toString()
 
-let styledHtml = _.template(template);
+const styledHtml = _.template(template)
 
-const mailTemplate = heml(
-  styledHtml(_.assign({}, style, templateContent)),
-  options
-).then(({ html, metadata, errors }) => {
-  //Write output mail template
-  writeFile(html, name);
-});
+heml(styledHtml(_.assign({}, style, templateContent)), options).then(
+  ({
+    html,
+    metadata,
+    errors
+  }) => {
+    // Write output mail template
+    writeFile(html, name)
+  }
+)
