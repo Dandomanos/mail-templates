1fdad544fb70b7cd608f6405a44e7e54248a3fdd
Dandomanos <dandomanos@gmail.com>
2018-04-06 13:41:36 +0200

    New: Get Final step content from configuration

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ import getPromoContent from './src/lib/getPromoContent'
 import writeFile from './src/writeFile'
 import _ from 'lodash'
 
-// Html template file name
-const name = process.argv[2] || 'email'
-
-if (!name) throw new Error('No template name defined')
-
 const options = {
   validate: 'soft', // validation levels - 'strict'|'soft'|'none'
   cheerio: {}, // config passed to cheerio parser
@@ -24,14 +19,21 @@ const options = {
 const debug = require('debug')('app:index')
 
 const participateResult = {
-  status: 'winner',
+  status: 'not winner',
   date: '1522999245280',
   prizeId: 273,
   type: 'tier',
   participationId: 585492,
+  promoUrl: 'https://origin-front-pt.s.orchextra.io/455',
 }
 
-const getTemplate = async (environment, promoId, lang, participate = participateResult) => {
+const getTemplate = async (
+  environment,
+  promoId,
+  lang,
+  participate,
+  templateName = 'participate'
+) => {
   // get configuration
   const promoConfig = await getConfiguration(environment, promoId)
   debug('promoConfig', promoConfig)
@@ -41,16 +43,16 @@ const getTemplate = async (environment, promoId, lang, participate = participate
   debug('promoStyle', promoStyle)
 
   // Get Promo Template
-  const template = fs.readFileSync(`${__dirname}/templates/${name}.heml`).toString()
+  const template = fs.readFileSync(`${__dirname}/templates/${templateName}.heml`).toString()
   const styledHtml = _.template(template)
 
   // Get Promo Content
-  const templateContent = getPromoContent()
+  const templateContent = getPromoContent(promoConfig, lang, participate)
 
   // Compile email with promoStyle and templateContent
   const mailTemplate = await heml(styledHtml(_.assign({}, promoStyle, templateContent)), options)
 
   // write
-  writeFile(mailTemplate.html, name)
+  writeFile(mailTemplate.html, templateName)
 }
 getTemplate('staging', 455, 'es', participateResult)
--- src/lib/getPromoContent.js
+++ src/lib/getPromoContent.js
@@ -1,11 +1,67 @@
-const getPromoContent = () => {
+import _ from 'lodash'
+// aux functions
+const translator = (obj, lang, fbLang) => {
+  return _.cloneDeepWith(obj, value => {
+    if (value && value.$ /* === 1 //TBD */) {
+      return value[lang] || value[fbLang] || value[getFirstLangKey(value)] || ''
+    }
+  })
+}
+const getFirstLangKey = value => {
+  return Object.keys(value).filter(x => x != '$')[0]
+}
+
+const debug = require('debug')('app:get-content')
+const getPromoContent = (promoConfig, lang, participate) => {
+  const status =
+    participate.status === 'winner' || participate.status === 'participant' ? 'OK' : 'Invalid'
+  const $pc = path => {
+    return translator(_.get(promoConfig, path, null), lang, 'en')
+  }
+  const redirectUrl = (template, type) => {
+    const active = $pc(`template.${template}.${type}.activeUrl`)
+    let url = $pc(`template.${template}.${type}.url`)
+
+    // Append voucher code if needed
+    const appendVoucher = $pc(`template.${template}.${type}.voucherInUrl`)
+    if (appendVoucher && participate.voucher) url += participate.voucher
+
+    return active && url ? url : false
+  }
+  const imageHeader =
+    $pc('template.home.headerDesktopImage') ||
+    $pc('template.home.headerMobileImage') ||
+    $pc('template.header.headerDesktopImage') ||
+    $pc('template.header.headerMobileImage')
+
+  const flow = _.get(promoConfig, 'steps', [])
+  const template = `${flow[flow.length - 1]}${status}`
+  const title = $pc(`template.${template}.title`)
+  const content = $pc(`template.${template}.content`)
+  const buttonText = $pc(`template.${template}.button.text`)
+  const secondaryButtonText = $pc(`template.${template}.secondaryButton.text`)
+  const buttonUrl = redirectUrl(template, 'button') || participate.promoUrl
+  const secondaryButtonUrl = redirectUrl(template, 'secondaryButton') || participate.promoUrl
+  const secondaryButton = secondaryButtonText
+    ? `<button class="secondary" href="${secondaryButtonUrl}">${secondaryButtonText}</button>`
+    : ''
+  debug('template', template)
+  debug('imageHeader', imageHeader)
+  debug('title', title)
+  debug('content', content)
+  debug('buttonUrl', buttonUrl)
+  debug('secondaryButtonUrl', secondaryButtonUrl)
+  debug('buttonText', buttonText)
+  debug('secondaryButtonText', secondaryButtonText)
+  debug('secondaryButton', secondaryButton)
+
   return {
-    imageHeader:
-      'https://s3-eu-west-1.amazonaws.com/orchextra-images-pt-dev/5a1fcc5cda4a93183c915032.jpeg',
-    title: "CONGRATULATIONS, YOU'RE A WINNER!",
-    description: 'You won an amazing prize. Click on the button below and claim your prize.',
-    buttonText: 'Claim Prize',
-    buttonUrl: 'https://heml.io',
+    imageHeader: imageHeader,
+    title: title,
+    content: content,
+    buttonText: buttonText,
+    buttonUrl: buttonUrl,
+    secondaryButton: secondaryButton,
   }
 }
 export default getPromoContent
